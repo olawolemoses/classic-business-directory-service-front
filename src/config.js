b266c0d1871c2ce04908ca05c903b60c083215a9
olawolemoses <olawolemoses@gmail.com>
2020-06-01 12:39:44 +0100

    production ready

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    "apiUrl": "http://blooming-bastion-89244.herokuapp.com/api",
-    "siteUrl": "http://blooming-bastion-89244.herokuapp.com/",
+    "apiUrl": "https://blooming-bastion-89244.herokuapp.com/api",
+    "siteUrl": "https://blooming-bastion-89244.herokuapp.com/",
 }
